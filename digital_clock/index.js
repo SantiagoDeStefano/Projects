ec3b9f69104028153f2eca1ac128fa66d5c62a3f
Furry_Hunter <142719621+SantiagoDeStefano@users.noreply.github.com>
2024-12-05 09:50:12 +0700

    Add files via upload

---
+++ digital_clock/index.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const clockWeb = document.getElementById("clock");
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+ 
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    const time = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    clockWeb.textContent = time;
+}
+updateClock();
+setInterval(updateClock, 1000);
